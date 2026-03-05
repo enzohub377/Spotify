7c80b5da8499dc528bbe253020c08198c0c08a43
enzohub377 <enzoideias01@gmail.com>
2026-03-04 21:53:51 -0300

    Primeiras alterações com JS, além de uma nova pasta de Imagens adiciona para a introdução dos cards.

---
+++ script.js
@@ -0,0 +1,17 @@
+const artistsData=[
+    {name:'Henrique & Juliano', image:'.Img/artista-henrique-juliano.jpg'},
+    {name:'Jorge & Mateus', image:'.Img/artista-jorge-mateus.jpg'},
+    {name:'Zé Neto & Cristiano', image:'.Img/artista-ze-neto.jpg'},
+    {name:'Gusttavo Lima', image:'.Img/artista-gustavo-limma.jpg'},
+    {name:'Luan Santana', image:'.Img/artista-luan-santana.jpg'},
+    {name:'Matheus & Kauan', image:'.Img/artista-mateus-kauan.jpg'}
+]
+
+const albumsData=[
+    {name:'White Noise (Sleep & Relaxation Sounds)', artist:'Sleepy John', image:'.Img/album-white-noise.jpg'},
+    {name:'O Céu Explica Tudo (Ao Vivo)', artist:'Henrique & Juliano', image:'.Img/album-ceu-explica.jpg'},
+    {name:'Nada como um dia após o outro', artist:'Racionais', image:'.Img/album-vida-loka.jpg'},
+    {name:'HIT ME HARD AND SOFT', artist:'Billie Eilish', image:'.Img/album-hit-me.jpg'},
+    {name:'CAJU', artist:'Liniker', image:'.Img/album-caju.jpg'},
+    {name:'Escândalo Íntimo', artist:'Luísa Sonza', image:'.Img/album-escandalo.jpg'}
+]
